17074c4c353f56c66eb73234d7e314d7667645d7
ShreyTanna29 <shreytanna2957@gmail.com>
2024-05-11 13:59:18 +0530

    cloudinary and multer config

--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@ mongoose-aggregate-paginate-v2
 bcrypt(for hashing passwords)
 jwt
 mongoose hooks (plugin, pre)
+multer
+servis=ces: cloudinary
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("file uploaded on clodinary", response);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // deletes the file locally
+    console.log("Error :: utils :: cloudinary.js :: ", error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
